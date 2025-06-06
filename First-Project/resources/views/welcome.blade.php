1874ec3f8d1d1e61466c9074fa6a26a7450eaeca
koushik <koushikbaju07@gmail.com>
2025-06-06 11:19:16 +0530

    8. Laravel Blade Template
    Basics of Blade Template

--- First-Project/resources/views/welcome.blade.php
+++ First-Project/resources/views/welcome.blade.php
@@ -1,14 +1,47 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Welcome Page</title>
-</head>
-<body>
-    <h1>Our First Page</h1>
-    <a href="{{route('mypost')}}">Post</a>
-    <a href="{{route('about')}}">About</a>
-</body>
-</html>
+<h1>This is our Home Page</h1>
+
+@php
+    $user = ["koushik",'Souvik',"Rishav","Debanjan","Rupa"]
+@endphp
+
+{{-- <ul>
+    @foreach ($user as $n)
+        <li>{{$loop->index}} {{$n}}</li>
+    @endforeach
+</ul> --}}
+
+{{-- <ul>
+    @foreach ($user as $n)
+        <li>{{$loop->iteration}} {{$n}}</li>
+    @endforeach
+</ul> --}}
+
+{{-- <ul>
+    @foreach ($user as $n)
+        <li>{{$loop->count}} {{$n}}</li>
+    @endforeach
+</ul> --}}
+
+{{-- <ul>
+    @foreach ($user as $n)
+        @if($loop->first)
+            <li style="color:red">{{$n}}</li>
+        @elseif($loop->last)
+            <li style="color:green">{{$n}}</li>
+        @else
+            <li>{{$n}}</li>
+        @endif
+    @endforeach
+</ul> --}}
+
+<ul>
+    @foreach ($user as $n)
+        @if($loop->even)
+            <li style="color:red">{{$n}}</li>
+        @elseif($loop->odd)
+            <li style="color:green">{{$n}}</li>
+        @else
+            <li>{{$n}}</li>
+        @endif
+    @endforeach
+</ul>
